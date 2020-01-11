fcebb063e5e3092da54980a80cfa11323b78242b
percyvega <percyvega@gmail.com>
2020-01-11 00:26:09 -0500

    3 more problems

---
+++ src/main/java/com/percyvega/nonhackerrank/SmallestPositiveIntegerNotFoundInArray.java
@@ -0,0 +1,29 @@
+package com.percyvega.nonhackerrank;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SmallestPositiveIntegerNotFoundInArray {
+
+    @Test
+    void testSmallestPositiveIntegerNotFoundInArray() {
+        assertThat(getSmallestInt(new int[]{10})).isEqualTo(1);
+        assertThat(getSmallestInt(new int[]{1, 3, 6, 4, 1, 2})).isEqualTo(5);
+        assertThat(getSmallestInt(new int[]{1, 2, 3})).isEqualTo(4);
+        assertThat(getSmallestInt(new int[]{-1, -3})).isEqualTo(1);
+    }
+
+    private int getSmallestInt(int[] A) {
+        Arrays.sort(A);
+        int i = 0;
+        while (true) {
+            if (Arrays.binarySearch(A, ++i) < 0) {
+                return i;
+            }
+        }
+    }
+
+}
